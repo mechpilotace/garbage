d2df3203007e88a4a5b3faf661a3baae1cd17575
mechpilotace <44477228+mechpilotace@users.noreply.github.com>
2019-01-18 18:42:27 -0500

    Create prog.py

---
+++ prog.py
@@ -0,0 +1,16 @@
+keepgoing = True
+  1 
+  2 while keepgoing:
+  3         print("What is your favorite colour?")
+  4         colourvalue = input("Enter Value: ")
+  5 
+  6         if colourvalue == "I am the supreme ruler of this realm!":
+  7                 keepgoing = False
+  8         elif colourvalue == "Green" or colourvalue == "green":
+  9                 print("I should kick your ass.")
+ 10         elif colourvalue == "Red" or colourvalue == "red":
+ 11                 print("-_-")
+ 12         elif colourvalue == "bazinga" or colourvalue == "Bazinga":
+ 13                 print("kys")
+ 14         else:
+ 15                 print("Is " + colourvalue + " your favorite color?")
